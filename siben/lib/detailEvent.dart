19787cd4506672f8c69a60c41cce1b29c394bb28
xramadhann <xxdhanz@gmail.com>
2024-05-09 02:15:42 +0700

    feat : adds new three languange's description

--- siben/lib/detailEvent.dart
+++ siben/lib/detailEvent.dart
@@ -1,27 +1,70 @@
-import 'package:audioplayers/audioplayers.dart';
+// ignore_for_file: library_private_types_in_public_api
+
 import 'package:flutter/material.dart';
+import 'package:siben/viewmodels/museumTitle.dart';
 
 class DetailEvent extends StatefulWidget {
-  const DetailEvent({Key? key}) : super(key: key);
+  const DetailEvent({Key? key, required this.museum}) : super(key: key);
+  final Museum museum;
 
   @override
   _DetailEventState createState() => _DetailEventState();
 }
 
-AudioPlayer _player = AudioPlayer(); // Create an audio player instance
-
 class _DetailEventState extends State<DetailEvent> {
-  String audioUrl =
-      "http://commondatastorage.googleapis.com/codeskulptor-assets/Collision8-Bit.ogg"; // Replace with your actual audio URL
-
+  String _currentSubtitle = '';
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () async {
-        var urlSource = UrlSource(audioUrl);
-        await _player.play(urlSource);
-      },
-      child: const Text("data"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.museum.title),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Image.asset(
+              widget.museum.assetImagePath,
+              height: 20,
+            ),
+          ),
+          Text(
+            _currentSubtitle.isNotEmpty
+                ? _currentSubtitle
+                : widget.museum.subtitle,
+            style: const TextStyle(fontSize: 16),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _currentSubtitle = widget.museum.subtitle;
+                  });
+                },
+                child: const Text('ind'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _currentSubtitle = widget.museum.subtitle2;
+                  });
+                },
+                child: const Text('eng'),
+              ),
+              const SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _currentSubtitle = widget.museum.subtitle3;
+                  });
+                },
+                child: const Text('mnd'),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
--- siben/lib/homepage.dart
+++ siben/lib/homepage.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors, library_private_types_in_public_api
 import 'package:flutter/material.dart';
 import 'package:siben/detailEvent.dart';
+import 'package:siben/viewmodels/museumTitle.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({Key? key}) : super(key: key);
@@ -10,60 +11,90 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  final controller = TextEditingController();
+
+  List<Museum> museums = allMuseums;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [
-          Text(
-            "data",
-            style: TextStyle(
-                color: Colors.amber, fontSize: 50, fontWeight: FontWeight.bold),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: GestureDetector(
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => DetailEvent()));
-              },
-              child: Container(
-                decoration: BoxDecoration(
+      body: Column(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.all(20),
+            child: TextField(
+              controller: controller,
+              decoration: InputDecoration(
+                prefixIcon: const Icon(Icons.search),
+                hintText: 'Museum Title',
+                border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(20),
-                  color: Colors.amber,
-                ),
-                height: 200,
-                child: const Text(
-                  "text",
-                  style: TextStyle(color: Colors.white),
-                  textAlign: TextAlign.center,
+                  borderSide: BorderSide(color: Colors.amber),
                 ),
               ),
+              onChanged: searchMuseum,
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: GestureDetector(
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => DetailEvent()));
-              },
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.amber,
-                ),
-                height: 200,
-                child: const Text(
-                  "text",
-                  style: TextStyle(color: Colors.white),
-                  textAlign: TextAlign.center,
-                ),
+          Expanded(
+            child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount:
+                    2, // Ubah sesuai dengan jumlah kolom yang Anda inginkan
+                crossAxisSpacing:
+                    10, // Spasi antar item di dalam grid secara horizontal
+                mainAxisSpacing:
+                    10, // Spasi antar item di dalam grid secara vertikal
               ),
+              itemCount: museums.length,
+              itemBuilder: (context, index) {
+                final museum = museums[index];
+                return GestureDetector(
+                  onTap: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => DetailEvent(museum: museum),
+                    ),
+                  ),
+                  child: Card(
+                    elevation: 3, // Elevasi kartu
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Expanded(
+                          child: Image.asset(
+                            museum.assetImagePath,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(20),
+                          child: Text(
+                            museum.title,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
             ),
           ),
         ],
       ),
     );
+  }
+
+  void searchMuseum(String query) {
+    final suggestions = allMuseums.where((museum) {
+      final museumTitle = museum.title.toLowerCase();
+      final input = query.toLowerCase();
+      return museumTitle.contains(input);
+    }).toList();
+    setState(() => museums = suggestions);
   }
 }
