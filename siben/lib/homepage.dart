dbd205fc8d4d8e5ca1066699409c9352700de2e4
xramadhann <xxdhanz@gmail.com>
2024-05-09 23:55:26 +0700

    feat : add new feature number pagination

--- siben/lib/homepage.dart
+++ siben/lib/homepage.dart
@@ -1,6 +1,7 @@
-// ignore_for_file: prefer_const_constructors, library_private_types_in_public_api
 import 'package:flutter/material.dart';
+import 'package:siben/detailEvent.dart';
 import 'package:siben/viewmodels/museumTitle.dart';
+import 'package:number_pagination/number_pagination.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({Key? key}) : super(key: key);
@@ -13,14 +14,26 @@ class _HomepageState extends State<Homepage> {
   final controller = TextEditingController();
 
   List<Museum> museums = allMuseums;
+  int itemsPerPage = 10; // Jumlah museum per halaman
+  int currentPage = 1;
+  int get numberOfPages => (museums.length / itemsPerPage).ceil();
 
   @override
   Widget build(BuildContext context) {
+    var startIndex = (currentPage - 1) * itemsPerPage;
+    var endIndex = startIndex + itemsPerPage;
+    endIndex = endIndex > museums.length ? museums.length : endIndex;
+
+    // Pastikan terdapat setidaknya satu item yang ditampilkan
+    if (startIndex >= museums.length) {
+      startIndex = museums.length - 1;
+    }
+
     return Scaffold(
       body: Column(
         children: <Widget>[
           Container(
-            margin: EdgeInsets.all(20),
+            margin: const EdgeInsets.all(20),
             child: TextField(
               controller: controller,
               decoration: InputDecoration(
@@ -28,28 +41,73 @@ class _HomepageState extends State<Homepage> {
                 hintText: 'Museum Title',
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(20),
-                  borderSide: BorderSide(color: Colors.amber),
+                  borderSide: const BorderSide(color: Colors.amber),
                 ),
               ),
               onChanged: searchMuseum,
             ),
           ),
           Expanded(
-            child: ListView.builder(
-              itemCount: museums.length,
-              itemBuilder: (context, index) {
-                final museum = museums[index];
-                return ListTile(
-                  leading: Image.network(
-                    museum.urlImage,
-                    fit: BoxFit.cover,
-                    width: 50,
-                    height: 50,
+            child: CustomScrollView(
+              slivers: <Widget>[
+                SliverGrid(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
                   ),
-                  title: Text(museum.title),
-                );
-              },
+                  delegate: SliverChildBuilderDelegate(
+                    (BuildContext context, int index) {
+                      final museumIndex = startIndex + index;
+                      final museum = museums[museumIndex];
+                      return GestureDetector(
+                        onTap: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => DetailEvent(museum: museum),
+                          ),
+                        ),
+                        child: Card(
+                          elevation: 3,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              Expanded(
+                                child: Image.asset(
+                                  museum.assetImagePath,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(20),
+                                child: Text(
+                                  museum.title,
+                                  textAlign: TextAlign.center,
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                    childCount: endIndex - startIndex,
+                  ),
+                ),
+              ],
             ),
+          ),
+          NumberPagination(
+            pageTotal: numberOfPages,
+            onPageChanged: (index) {
+              setState(() {
+                currentPage = index;
+              });
+            },
+            pageInit: currentPage,
           )
         ],
       ),
@@ -62,6 +120,9 @@ class _HomepageState extends State<Homepage> {
       final input = query.toLowerCase();
       return museumTitle.contains(input);
     }).toList();
-    setState(() => museums = suggestions);
+    setState(() {
+      museums = suggestions;
+      currentPage = 1; // Kembali ke halaman pertama setiap kali mencari
+    });
   }
 }
