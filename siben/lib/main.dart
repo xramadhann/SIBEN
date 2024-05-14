13468413376e5403fe02c2de35c2e1e6b93ad550
AgentGalon <zahranrafif911@gmail.com>
2024-05-15 00:13:49 +0700

    feat: add new splash screen

--- siben/lib/main.dart
+++ siben/lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:siben/view/homepage.dart';
 
-void main() {
+void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await Future.delayed(const Duration(seconds: 3));
+  FlutterNativeSplash.remove();
   runApp(const MyApp());
 }
 
