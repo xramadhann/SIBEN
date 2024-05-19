37ab26204262f86c471a9f14e70dbe432f6a64d3
xramadhann <xxdhanz@gmail.com>
2024-05-19 20:29:26 +0700

    feat : add ui card filter

--- siben/lib/view/homepage.dart
+++ siben/lib/view/homepage.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: unnecessary_import, unnecessary_const
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
@@ -15,167 +16,156 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Container(
-            height: 210,
-            decoration: BoxDecoration(
-              color: Colors.orange[300],
-              borderRadius: const BorderRadius.only(
-                bottomLeft: Radius.circular(20),
-                bottomRight: Radius.circular(20),
-              ),
-            ),
-            child: Column(
-              children: [
-                const Text(
-                  "Hi Selamat Datang!",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white),
+      body: Expanded(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 210,
+              decoration: BoxDecoration(
+                color: Colors.orange[300],
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(20),
+                  bottomRight: Radius.circular(20),
                 ),
-                Container(
-                  margin: const EdgeInsets.all(20),
-                  padding: const EdgeInsets.only(bottom: 50),
-                  child: SearchBar(
-                    hintText: "Search",
-                    onChanged: _controller.searchMuseum,
-                    padding: MaterialStateProperty.all<EdgeInsets>(
-                      const EdgeInsets.symmetric(horizontal: 15),
+              ),
+              child: Column(
+                children: [
+                  const SizedBox(height: 70),
+                  Container(
+                    margin: const EdgeInsets.only(right: 80),
+                    child: const Text(
+                      "HI, Selamat datang di siben!",
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
                     ),
-                    backgroundColor: MaterialStateProperty.all(Colors.white),
-                    leading: const Icon(Icons.search),
                   ),
-                ),
-              ],
+                  Container(
+                    margin: const EdgeInsets.only(top: 15, left: 20, right: 20),
+                    child: SearchBar(
+                      hintText: "Search",
+                      onChanged: _controller.searchMuseum,
+                      padding: MaterialStateProperty.all<EdgeInsets>(
+                        const EdgeInsets.symmetric(horizontal: 15),
+                      ),
+                      backgroundColor: MaterialStateProperty.all(Colors.white),
+                      leading: const Icon(Icons.search),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          const Row(
-            children: <Widget>[
-              Card(
-                margin: EdgeInsets.all(20),
-                child: Column(
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.all(50),
-                    ),
-                    const Icon(Icons.safety_check),
-                  ],
-                ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10, left: 10, right: 10),
+              padding: EdgeInsets.only(left: 15, right: 15),
+              height: 120,
+              width: 500,
+              child: Row(
+                children: [
+                  Column(
+                    children: [
+                      Card(
+                        margin: const EdgeInsets.all(10),
+                        child: Container(
+                          margin: const EdgeInsets.all(35),
+                        ),
+                      ),
+                      Text("data 1"),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Card(
+                        margin: const EdgeInsets.all(10),
+                        child: Container(
+                          margin: const EdgeInsets.all(35),
+                        ),
+                      ),
+                      Text("data 1"),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Card(
+                        margin: const EdgeInsets.all(10),
+                        child: Container(
+                          margin: const EdgeInsets.all(35),
+                        ),
+                      ),
+                      Text("data 1"),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Card(
+                        margin: const EdgeInsets.all(10),
+                        child: Container(
+                          margin: const EdgeInsets.all(35),
+                        ),
+                      ),
+                      Text("data 1"),
+                    ],
+                  ),
+                ],
               ),
-              Card(
-                margin: EdgeInsets.all(20),
-                child: Column(
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.all(50),
-                    ),
-                    Text(
-                      "Piagam",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
+            ),
+            Expanded(
+              child: Obx(
+                () => GridView.builder(
+                  padding: const EdgeInsets.all(20),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 13 / 16,
+                    crossAxisSpacing: 20,
+                    mainAxisSpacing: 20,
+                  ),
+                  itemCount: _controller.museums.length,
+                  itemBuilder: (context, index) {
+                    final museum = _controller.museums[index];
+                    return GestureDetector(
+                      onTap: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => DetailEvent(museum: museum),
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-              ),
-              Card(
-                margin: EdgeInsets.all(20),
-                child: Column(
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.all(50),
-                    ),
-                    Text(
-                      "Piagam",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Card(
-                margin: EdgeInsets.all(20),
-                child: Column(
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.all(50),
-                    ),
-                    Text(
-                      "Piagam",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            ],
-          ),
-          const SizedBox(height: 20),
-          Expanded(
-            child: Obx(
-              () => GridView.builder(
-                padding: const EdgeInsets.all(20),
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  childAspectRatio: 16 / 13,
-                  crossAxisSpacing: 20,
-                  mainAxisSpacing: 20,
-                ),
-                itemCount: _controller.museums.length,
-                itemBuilder: (context, index) {
-                  final museum = _controller.museums[index];
-                  return GestureDetector(
-                    onTap: () => Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => DetailEvent(museum: museum),
-                      ),
-                    ),
-                    child: Card(
-                      elevation: 1,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          Expanded(
-                            child: Image.asset(
-                              museum.assetImagePath,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(10),
-                            child: Text(
-                              museum.title,
-                              textAlign: TextAlign.center,
-                              style: const TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
+                      child: Card(
+                        elevation: 1,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Expanded(
+                              child: Image.asset(
+                                museum.assetImagePath,
+                                fit: BoxFit.cover,
                               ),
                             ),
-                          ),
-                        ],
+                            Padding(
+                              padding: const EdgeInsets.all(10),
+                              child: Text(
+                                museum.title,
+                                textAlign: TextAlign.center,
+                                style: const TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  );
-                },
+                    );
+                  },
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
